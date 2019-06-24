abf7246b99cda2329d2bf43fa3aba4c39748da91
nirmalapooja <48995874+nirmalapooja@users.noreply.github.com>
2019-06-24 18:27:49 -0500

    Add files via upload
    added latest

--- Employee_Table_schemata.sql
+++ Employee_Table_schemata.sql
@@ -19,6 +19,17 @@ CREATE TABLE "departments" (
      )
 );
 
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
     "dept_no" VARCHAR   NOT NULL,
@@ -33,17 +44,6 @@ CREATE TABLE "dept_manager" (
     "to_date" DATE   NOT NULL
 );
 
-CREATE TABLE "employees" (
-    "emp_no" INT   NOT NULL,
-    "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "gender" VARCHAR   NOT NULL,
-    "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY (
-        "emp_no"
-     )
-);
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
